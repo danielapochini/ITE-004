6de2be5c7cfe0a8aee1ab5dc0fd6050b708fd77b
danpochini <pocchini@live.dk>
2017-02-02 15:17:21 -0200

    Exemplo de uso Singleton

---
+++ designpatterns/singleton/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+	public static void main(String[] args) {
+		Singleton s = Singleton.getInstance();
+		Singleton t = Singleton.getInstance();
+		System.out.println(s);
+		System.out.println(t);
+	}
+
+}
